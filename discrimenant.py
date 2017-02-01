122fe00665e26ef1c640736bf419f41c13d32ef7
Bebekalka <nda_mine@mail.ru>
2017-02-01 20:22:56 +0400

    Update discrimenant.py

--- discrimenant.py
+++ discrimenant.py
@@ -3,94 +3,110 @@ from Tkinter import*
 root = Tk()
 root.geometry("240x145")
 def prov1():
-    a = en3.get()
-    minus = 0
+    a = en1.get()
     toch = 0
-    if a != "":
+    if a != ""  and a!= "-" and a != "+" and a != ".":
         for i in range(len(a)):
-            if (a[i] >= "0" and a[i] <= "9") or a[i] == "-" or a[i] == ".":
+            if (a[i] >= "0" and a[i] <= "9") or a[i] == "-" or a[i] == "." or a[i] == "+":
                 if a[i] == "-":
                     if i != 0:
                         lberr.config(text = "В A минус не там")
                         return False
-                    if a[i] == ".":
-                        toch += 1
+                if a[i] == "+":
+                    if  i != 0:
+                        lberr.config(text = "В A плюс не там")
+                        return False
+                if a[i] == ".":
+                    toch += 1
             else:
                 lberr.config(text = "Я не умею считать буквы")
                 return False
             if toch > 1:
                 lberr.config(text = "В A много точек")
+                return False
         return True
     else:
-       lberr.config(text = "В A нету ничего") 
+       lberr.config(text = "В A нету ничего")
 def prov2():
-    a = en3.get()
-    minus = 0
+    a = en2.get()
     toch = 0
     if a != "":
         for i in range(len(a)):
-            if (a[i] >= "0" and a[i] <= "9") or a[i] == "-" or a[i] == ".":
+            if (a[i] >= "0" and a[i] <= "9") or a[i] == "-" or a[i] == "." or a[i] == "+":
                 if a[i] == "-":
                     if i != 0:
                         lberr.config(text = "В B минус не там")
                         return False
-                    if a[i] == ".":
-                        toch += 1
+                if a[i] == "+":
+                    if  i != 0:
+                        lberr.config(text = "В B плюс не там")
+                        return False
+                if a[i] == ".":
+                    toch += 1
             else:
                 lberr.config(text = "Я не умею считать буквы")
                 return False
             if toch > 1:
                 lberr.config(text = "В B много точек")
+                return False
         return True
     else:
-       lberr.config(text = "В B нету ничего") 
+       lberr.config(text = "В B нету ничего")
+
 def prov3():
     a = en3.get()
-    minus = 0
     toch = 0
     if a != "":
         for i in range(len(a)):
-            if (a[i] >= "0" and a[i] <= "9") or a[i] == "-" or a[i] == ".":
+            if (a[i] >= "0" and a[i] <= "9") or a[i] == "-" or a[i] == "." or a[i] == "+":
                 if a[i] == "-":
                     if i != 0:
                         lberr.config(text = "В C минус не там")
                         return False
-                    if a[i] == ".":
-                        toch += 1
+                if a[i] == "+":
+                    if  i != 0:
+                        lberr.config(text = "В C плюс не там")
+                        return False
+                if a[i] == ".":
+                    toch += 1
             else:
                 lberr.config(text = "Я не умею считать буквы")
                 return False
             if toch > 1:
                 lberr.config(text = "В C много точек")
+                return False
         return True
     else:
-       lberr.config(text = "В C нету ничего") 
+       lberr.config(text = "В C нету ничего")
 def discr():
     D = (float(en2.get())**2)-(4*float(en1.get())*float(en3.get()))
-    
     return D
+
 def reshit():
     if prov1():
         if prov2():
             if prov3():
                 D = discr()
+                D = round(D, 4)
                 if D >= 0:
                     x1 = (-1*float(en2.get())+(D**0.5))/(2*float(en1.get()))
                     x2 = (-1*float(en2.get())-(D**0.5))/(2*float(en1.get()))
-                    lbx1.config(text="x1="+str(x1))
-                    lbx2.config(text="x2="+str(x2))
+                    lbx1.config(text="x1="+str(round(x1,4)))
+                    lbx2.config(text="x2="+str(round(x2,4)))
                     lbdisc.config(text="Дискриминант:"+str(D))
                 else:
                     lberr.config(text="Дискриминант меньше 0")
-                    lbx1.config(text="x1єØ"+str(x1))
-                    lbx2.config(text="x2єØ"+str(x2))
+                    lbx1.config(text="x1єØ")
+                    lbx2.config(text="x2єØ")
+                    lbdisc.config(text="Дискриминант:"+str(D))
+
 labmain = Label(root, width=11, height=1, text="Ax^2+Bx+C=0", cursor="heart")
 lb1 = Label(root, width=3, height=1, text="A=", cursor="heart")
 lb2 = Label(root, width=3, height=1, text="B=", cursor="heart")
 lb3 = Label(root, width=3, height=1, text="C=", cursor="heart")
 lberr = Label(root, width=20, height=1, cursor="heart")
-lbx1 = Label(root, width=6, height=1, cursor="heart")
-lbx2 = Label(root, width=6, height=1, cursor="heart")
+lbx1 = Label(root, width=8, height=1, cursor="heart")
+lbx2 = Label(root, width=8, height=1, cursor="heart")
 lbdisc = Label(root, width=15, height=1, cursor="heart")
 en1 = Entry(root, width=7, cursor="heart")
 en2 = Entry(root, width=7, cursor="heart")
@@ -109,4 +125,5 @@ en3.place(x=20, y=73)
 lberr.place(x=70, y=33)
 lbdisc.place(x=70, y=113)
 doit.place(x=10, y=103)
+
 root.mainloop()
